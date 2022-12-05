c8c4639caae4ed6424d242ee5799723612ffbb81
Madiyar Kuandyk <2003madi2003@gmail.com>
2022-12-05 19:59:13 +0600

    first commit with beep

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 
+import time
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtGui import QPainter, QFontMetricsF,QPen,QColor, QPalette, QFont, QPolygon
+from PySide2.QtGui import QPainter, QFontMetricsF, QPen, QColor, QPalette, QFont, QPolygon
 from PySide2.QtWidgets import QMainWindow, QWidget, QScrollArea, QVBoxLayout, QApplication, QGraphicsDropShadowEffect, \
     QSizeGrip, QSizePolicy, QFrame, QPushButton, QLabel, QSpinBox
 from PySide2.QtCharts import QtCharts
 from PySide2.QtCore import QPropertyAnimation, QSize, QTimer, Qt, Signal, QRect, QCoreApplication, QPoint
 import pyqtgraph as pg
-from PySide2.QtCore import QPropertyAnimation, QSize, QTimer, Qt, QRect, QCoreApplication
 from random import randrange
 from functools import partial
 import csv
@@ -18,8 +18,9 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import networkx as nx
 from PyQt5.QtCore import pyqtSlot as pyqtSlot, pyqtProperty as pyqtProperty
-
+import scan
 from ui_interface import Ui_MainWindow
+import threading
 
 shadow_elements = {
     "left_menu_frame",
@@ -32,7 +33,8 @@ shadow_elements = {
 
 def read_wifipoints():
     global wifi_points
-    with open('data.csv', 'r') as file:
+    scan.general_scan()
+    with open('data-01.csv', 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
         next(csv_reader)
@@ -78,6 +80,7 @@ def toogle_button(obj):
         obj.x.clear()
         obj.y.clear()
         obj.timer.stop()
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -188,10 +191,10 @@ class MainWindow(QMainWindow):
             effect.setColor(QColor(0, 0, 0, 255))
             getattr(self.ui, x).setGraphicsEffect(effect)
 
-        self.ui.tools_btn.clicked.connect(lambda: read_wifipoints())
-        self.ui.topology_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.topology))
+        #self.ui.tools_btn.clicked.connect(lambda: read_wifipoints())
+        #self.ui.topology_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.topology))
         self.ui.nested_donut_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.nested_donuts))
-        self.ui.wifi_page_btn.clicked.connect(partial(link_buttons, self.ui, "No Data"))
+        #self.ui.wifi_page_btn.clicked.connect(partial(link_buttons, self.ui, "No Data"))
         # self.ui.wifi_page_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.wifi_page))
         self.ui.list_wifi_btn.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.list_wifi))
 
